e8f3e04476ec5b79e0c40e2af7e56c6019a1a7cf
Immanuel Christian Yobel <christ.yobel@gmail.com>
2024-08-01 15:56:44 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PythonProgress',
+    version='0.1',
+    packages='',
+    install_requires=[
+        # List your dependencies here
+    ],
+    author='Christian Yobel',
+    author_email='christ.yobel@gmail.com',
+    description='A Python progress bar library',
+    url='https://github.com/Christian-Yobel/Projects',
+)
