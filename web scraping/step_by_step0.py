87294a48f59e4b1dd81e0f086b6db1e6809f6b66
Immanuel Christian Yobel <christ.yobel@gmail.com>
2024-08-02 17:06:13 +0700

    Update step_by_step0.py

--- web scraping/step_by_step0.py
+++ web scraping/step_by_step0.py
@@ -5,6 +5,10 @@ import time
 
 base_url = "https://carisinyal.com/hp/?_sfm_harga_status=Available&_sfm_layar_ukuran_layar=1.77+14.61&_sfm_baterai_kapasitas_baterai=0+32000"
 
-soup = BeautifulSoup(base_url.content, 'html.parser')
+response = requests.get(base_url)
+soup = BeautifulSoup(response.content, 'html.parser')
+
 # Find all product containers
 products = soup.select('div.oxy-post')  # Adjust this selector if needed
+
+print(products)
